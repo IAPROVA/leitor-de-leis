96600655d22520302d5f87bb286ed6ecdf7e030c
IAPROVA <fredbelzs2020@gmail.com>
2026-09-13 13:52:48 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "leitor-de-leis-v1";
+const CACHE = "leitor-de-leis-v2";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
